df1487d77b7609bbaf287b58014e6d901fe228bc
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-01-22 00:47:59 -0500

    Use dirname

--- extract.js
+++ extract.js
@@ -1,8 +1,7 @@
-'use strict'
-
 import got from 'got'
 import { join } from 'path'
 import { mkdir, stat, writeFile } from 'fs/promises'
+import __dirname from './dirname.js'
 
 const verbose = process.argv.includes('-verbose')
 
@@ -10,7 +9,7 @@ export async function extract (response, live = true) {
   const date = new Date(response.headers.date)
   const z00 = value => value.toString().padStart(2, '0')
   const dayName = `${date.getFullYear()}.${z00(date.getMonth() + 1)}.${z00(date.getDate())}`
-  const dayFolderPath = join(process.cwd(), 'data', dayName)
+  const dayFolderPath = join(__dirname, 'data', dayName)
 
   const jsonFolderPath = join(dayFolderPath, z00(date.getHours()))
   await mkdir(jsonFolderPath, { recursive: true })
