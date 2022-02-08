b86761aa75aef9ac6e3b5f43716b09cdeacb1c02
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-02-08 07:29:09 -0500

    Improve data

--- web/index.js
+++ web/index.js
@@ -1,7 +1,34 @@
 import { punybind } from './punybind.js'
 
-const data = punybind(document.body, {
-  date: new Date(),
-  dateColor: 'gray',
-  temps: [19, 19, 19, 19, 19, 19.5, 19.5, 20, 20, 20.5, 20.5, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 20.5, 20]
-})
+(async function () {
+  const data = await punybind(document.body, {
+    date: new Date(),
+    dateColor: 'gray',
+    temps: [
+      /* 00:00 */ { real: 19 },
+      /* 01:00 */ { real: 19 },
+      /* 02:00 */ { real: 19 },
+      /* 03:00 */ { real: 19 },
+      /* 04:00 */ { real: 19 },
+      /* 05:00 */ { real: 19.5 },
+      /* 06:00 */ { real: 19.5 },
+      /* 07:00 */ { real: 20 },
+      /* 08:00 */ { real: 20 },
+      /* 09:00 */ { real: 20.5 },
+      /* 10:00 */ { real: 20.5 },
+      /* 11:00 */ { real: 21 },
+      /* 12:00 */ { real: 21 },
+      /* 13:00 */ { real: 21 },
+      /* 14:00 */ { real: 21 },
+      /* 15:00 */ { real: 21 },
+      /* 16:00 */ { real: 21 },
+      /* 17:00 */ { real: 21 },
+      /* 18:00 */ { real: 21 },
+      /* 19:00 */ { real: 21 },
+      /* 20:00 */ { real: 21 },
+      /* 21:00 */ { real: 21 },
+      /* 22:00 */ { real: 21 },
+      /* 23:00 */ { real: 20 }
+    ]
+  })
+}())
